c16bb1d52ceb1c8a770e02d2a2f6513cea2cefd5
Vanessa Carvalho Lima <73398589+Vnscarli@users.noreply.github.com>
2023-12-07 15:18:54 -0300

    Add probability for a 2 dice roll

--- RollTheDice.cpp
+++ RollTheDice.cpp
@@ -3,10 +3,17 @@
 //This code calculates the probability for a single dice roll
 
 int main(){
-    int lados = 20; //How many faces does the dice have?
-    int dice = 1; //How many dices are going to be rolled?
+    float faces = 20; //How many faces the dice has
+    int dice = 2; //How many dices are going to be rolled
+    float target = 18; //Target value for the test
     if(dice==1){
-        float resp = 1.0 / lados;
+        float resp = (faces-target+1) / faces; //probability for target or higher
+        printf("%f\n", resp);
+    }
+    else if(dice==2){
+        float x = (target*2)-1; //First element of arithmetic progression
+        float y = (faces*2)-1; //Last element of arithmetic progression
+        float resp = (((x+y)*(faces-target+1))/2)/(faces*faces);//probability for target or higher
         printf("%f\n", resp);
     }
     return 0;
